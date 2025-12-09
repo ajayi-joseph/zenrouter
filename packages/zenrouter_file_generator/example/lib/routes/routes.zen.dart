2f31baf7bef304b5832dd648cdb5a80dba6dd4c8
definev <daiduong.workmail@gmail.com>
2025-12-10 00:17:33 +0700

    style: Reformat generated route code for improved readability with multi-line lists and function calls.

--- packages/zenrouter_file_generator/example/lib/routes/routes.zen.dart
+++ packages/zenrouter_file_generator/example/lib/routes/routes.zen.dart
@@ -57,16 +57,26 @@ abstract class AppRoute extends RouteTarget with RouteUnique {}
 class AppCoordinator extends Coordinator<AppRoute> {
   final NavigationPath<AppRoute> authPath = NavigationPath('Auth');
   final IndexedStackPath<AppRoute> tabsPath = IndexedStackPath([
-    FeedTabLayout(), TabProfileRoute(), TabSettingsRoute(),
+    FeedTabLayout(),
+    TabProfileRoute(),
+    TabSettingsRoute(),
   ], 'Tabs');
   final IndexedStackPath<AppRoute> feedTabPath = IndexedStackPath([
-    FollowingLayout(), ForYouLayout(),
+    FollowingLayout(),
+    ForYouLayout(),
   ], 'FeedTab');
   final NavigationPath<AppRoute> followingPath = NavigationPath('Following');
   final NavigationPath<AppRoute> forYouPath = NavigationPath('ForYou');
 
   @override
-  List<StackPath> get paths => [root, authPath, tabsPath, feedTabPath, followingPath, forYouPath];
+  List<StackPath> get paths => [
+    root,
+    authPath,
+    tabsPath,
+    feedTabPath,
+    followingPath,
+    forYouPath,
+  ];
 
   @override
   void defineLayout() {
@@ -82,10 +92,17 @@ class AppCoordinator extends Coordinator<AppRoute> {
     return switch (uri.pathSegments) {
       [] => IndexRoute(),
       ['tabs', 'feed', 'for-you', 'sheet'] => ForYouSheetRoute(),
-      ['tabs', 'feed', 'following', final postId] => FeedPostRoute(postId: postId),
+      ['tabs', 'feed', 'following', final postId] => FeedPostRoute(
+        postId: postId,
+      ),
       ['tabs', 'feed', 'following'] => FollowingRoute(),
       ['tabs', 'feed', 'for-you'] => ForYouRoute(),
-      ['profile', final profileId, 'collections', final collectionId] => CollectionsCollectionIdRoute(profileId: profileId, collectionId: collectionId, queries: uri.queryParameters),
+      ['profile', final profileId, 'collections', final collectionId] =>
+        CollectionsCollectionIdRoute(
+          profileId: profileId,
+          collectionId: collectionId,
+          queries: uri.queryParameters,
+        ),
       ['profile', 'general'] => ProfileGeneralRoute(),
       ['tabs', 'profile'] => TabProfileRoute(),
       ['tabs', 'settings'] => TabSettingsRoute(),
@@ -93,9 +110,13 @@ class AppCoordinator extends Coordinator<AppRoute> {
       ['login'] => LoginRoute(),
       ['register'] => RegisterRoute(),
       ['about'] => AboutRoute(),
-      ['tabs', 'feed', 'following', ...final slugs, 'about'] => FeedDynamicAboutRoute(slugs: slugs),
-      ['tabs', 'feed', 'following', ...final slugs, final id] => FeedDynamicIdRoute(slugs: slugs, id: id),
-      ['tabs', 'feed', 'following', ...final slugs] => FeedDynamicRoute(slugs: slugs),
+      ['tabs', 'feed', 'following', ...final slugs, 'about'] =>
+        FeedDynamicAboutRoute(slugs: slugs),
+      ['tabs', 'feed', 'following', ...final slugs, final id] =>
+        FeedDynamicIdRoute(slugs: slugs, id: id),
+      ['tabs', 'feed', 'following', ...final slugs] => FeedDynamicRoute(
+        slugs: slugs,
+      ),
       _ => NotFoundRoute(uri: uri, queries: uri.queryParameters),
     };
   }
@@ -115,25 +136,71 @@ extension AppCoordinatorNav on AppCoordinator {
   Future<T?> pushIndex<T extends Object>() => push(IndexRoute());
   void replaceIndex() => replace(IndexRoute());
   void recoverIndex() => recover(IndexRoute());
-  Future<T?> pushCollectionsCollectionId<T extends Object>(String profileId, String collectionId, [Map<String, String> queries = const {}]) => push(CollectionsCollectionIdRoute(profileId: profileId, collectionId: collectionId, queries: queries));
-  void replaceCollectionsCollectionId(String profileId, String collectionId, [Map<String, String> queries = const {}]) => replace(CollectionsCollectionIdRoute(profileId: profileId, collectionId: collectionId, queries: queries));
-  void recoverCollectionsCollectionId(String profileId, String collectionId, [Map<String, String> queries = const {}]) => recover(CollectionsCollectionIdRoute(profileId: profileId, collectionId: collectionId, queries: queries));
-  Future<T?> pushProfileId<T extends Object>(String profileId) => push(ProfileIdRoute(profileId: profileId));
-  void replaceProfileId(String profileId) => replace(ProfileIdRoute(profileId: profileId));
-  void recoverProfileId(String profileId) => recover(ProfileIdRoute(profileId: profileId));
-  Future<T?> pushProfileGeneral<T extends Object>() => push(ProfileGeneralRoute());
+  Future<T?> pushCollectionsCollectionId<T extends Object>(
+    String profileId,
+    String collectionId, [
+    Map<String, String> queries = const {},
+  ]) => push(
+    CollectionsCollectionIdRoute(
+      profileId: profileId,
+      collectionId: collectionId,
+      queries: queries,
+    ),
+  );
+  void replaceCollectionsCollectionId(
+    String profileId,
+    String collectionId, [
+    Map<String, String> queries = const {},
+  ]) => replace(
+    CollectionsCollectionIdRoute(
+      profileId: profileId,
+      collectionId: collectionId,
+      queries: queries,
+    ),
+  );
+  void recoverCollectionsCollectionId(
+    String profileId,
+    String collectionId, [
+    Map<String, String> queries = const {},
+  ]) => recover(
+    CollectionsCollectionIdRoute(
+      profileId: profileId,
+      collectionId: collectionId,
+      queries: queries,
+    ),
+  );
+  Future<T?> pushProfileId<T extends Object>(String profileId) =>
+      push(ProfileIdRoute(profileId: profileId));
+  void replaceProfileId(String profileId) =>
+      replace(ProfileIdRoute(profileId: profileId));
+  void recoverProfileId(String profileId) =>
+      recover(ProfileIdRoute(profileId: profileId));
+  Future<T?> pushProfileGeneral<T extends Object>() =>
+      push(ProfileGeneralRoute());
   void replaceProfileGeneral() => replace(ProfileGeneralRoute());
   void recoverProfileGeneral() => recover(ProfileGeneralRoute());
-  Future<T?> pushFeedDynamicId<T extends Object>(List<String> slugs, String id) => push(FeedDynamicIdRoute(slugs: slugs, id: id));
-  void replaceFeedDynamicId(List<String> slugs, String id) => replace(FeedDynamicIdRoute(slugs: slugs, id: id));
-  void recoverFeedDynamicId(List<String> slugs, String id) => recover(FeedDynamicIdRoute(slugs: slugs, id: id));
-  Future<T?> pushFeedDynamicAbout<T extends Object>(List<String> slugs) => push(FeedDynamicAboutRoute(slugs: slugs));
-  void replaceFeedDynamicAbout(List<String> slugs) => replace(FeedDynamicAboutRoute(slugs: slugs));
-  void recoverFeedDynamicAbout(List<String> slugs) => recover(FeedDynamicAboutRoute(slugs: slugs));
-  Future<T?> pushFeedDynamic<T extends Object>(List<String> slugs) => push(FeedDynamicRoute(slugs: slugs));
-  void replaceFeedDynamic(List<String> slugs) => replace(FeedDynamicRoute(slugs: slugs));
-  void recoverFeedDynamic(List<String> slugs) => recover(FeedDynamicRoute(slugs: slugs));
-  Future<T?> pushFeedPost<T extends Object>(String postId) => push(FeedPostRoute(postId: postId));
+  Future<T?> pushFeedDynamicId<T extends Object>(
+    List<String> slugs,
+    String id,
+  ) => push(FeedDynamicIdRoute(slugs: slugs, id: id));
+  void replaceFeedDynamicId(List<String> slugs, String id) =>
+      replace(FeedDynamicIdRoute(slugs: slugs, id: id));
+  void recoverFeedDynamicId(List<String> slugs, String id) =>
+      recover(FeedDynamicIdRoute(slugs: slugs, id: id));
+  Future<T?> pushFeedDynamicAbout<T extends Object>(List<String> slugs) =>
+      push(FeedDynamicAboutRoute(slugs: slugs));
+  void replaceFeedDynamicAbout(List<String> slugs) =>
+      replace(FeedDynamicAboutRoute(slugs: slugs));
+  void recoverFeedDynamicAbout(List<String> slugs) =>
+      recover(FeedDynamicAboutRoute(slugs: slugs));
+  Future<T?> pushFeedDynamic<T extends Object>(List<String> slugs) =>
+      push(FeedDynamicRoute(slugs: slugs));
+  void replaceFeedDynamic(List<String> slugs) =>
+      replace(FeedDynamicRoute(slugs: slugs));
+  void recoverFeedDynamic(List<String> slugs) =>
+      recover(FeedDynamicRoute(slugs: slugs));
+  Future<T?> pushFeedPost<T extends Object>(String postId) =>
+      push(FeedPostRoute(postId: postId));
   void replaceFeedPost(String postId) => replace(FeedPostRoute(postId: postId));
   void recoverFeedPost(String postId) => recover(FeedPostRoute(postId: postId));
   Future<T?> pushFollowing<T extends Object>() => push(FollowingRoute());
